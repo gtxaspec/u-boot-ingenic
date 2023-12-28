24f95e221e2fbd5ee24f91cb03d360af37a67cc3
gtxaspec <gtxent@gmail.com>
2023-12-28 13:14:12 -0800

    mtdtest continued

--- common/cmd_sf.c
+++ common/cmd_sf.c
@@ -87,7 +87,7 @@ static ulong bytes_per_second(unsigned int len, ulong start_ms)
 }
 
 // Function declaration
-int process_spi_flash_data(struct spi_flash *flash);
+//int process_spi_flash_data(struct spi_flash *flash);
 
 // Constants
 #define SQUASHFS_MAGIC_OFFSET    0
@@ -104,19 +104,19 @@ uint64_t align_to_erase_block(uint64_t size) {
 }
 
 int process_spi_flash_data(struct spi_flash *flash) {
-    printf("Starting process_spi_flash_data\n");
+    //printf("Starting process_spi_flash_data\n");
 
     // Starting address is right after the kernel, adjust as necessary
     unsigned int start_addr = 0x250000;
     unsigned int end_addr = 0x360000; // Adjust as necessary
     unsigned int addr; // Declare outside the for loop for compatibility
 
-    printf("Searching SquashFS from 0x%X to 0x%X\n", start_addr, end_addr);
+    //printf("Searching SquashFS from 0x%X to 0x%X\n", start_addr, end_addr);
 
     for (addr = start_addr; addr < end_addr; addr += ERASE_BLOCK_SIZE) {
         char buf[64];
 
-        printf("Reading from address 0x%X\n", addr);
+        //printf("Reading from address 0x%X\n", addr);
         if (spi_flash_read(flash, addr, sizeof(buf), buf)) {
             printf("Failed to read from SPI flash at 0x%X\n", addr);
             continue; // Skip to the next block
@@ -125,17 +125,24 @@ int process_spi_flash_data(struct spi_flash *flash) {
         uint32_t magic_number;
         memcpy(&magic_number, buf + SQUASHFS_MAGIC_OFFSET, sizeof(magic_number));
 
-        printf("Magic number at 0x%X: 0x%08X\n", addr, magic_number);
+        //printf("Magic number at 0x%X: 0x%08X\n", addr, magic_number);
 
         if (magic_number == 0x73717368) {
             printf("SquashFS found at 0x%X\n", addr);
 
-            // Calculate kernel size
-            uint64_t kernel_size = addr - KERNEL_START_ADDR;
-            uint64_t aligned_kernel_size = align_to_erase_block(kernel_size);
-            char kern_size_str[32];
-            sprintf(kern_size_str, "%lluk", aligned_kernel_size / 1024);
-            setenv("kernsize", kern_size_str);
+			// Calculate kernel size
+			uint64_t kernel_size = addr - KERNEL_START_ADDR;
+			uint64_t aligned_kernel_size = align_to_erase_block(kernel_size);
+
+			// Store the size in kilobytes in 'kern_size'
+			char kern_size_str[32];
+			sprintf(kern_size_str, "%lluk", aligned_kernel_size / 1024); // Convert to kilobytes
+			setenv("kern_size", kern_size_str);
+
+			// Store the length in hexadecimal in 'kern_length'
+			char kern_length_str[32];
+			sprintf(kern_length_str, "%llx", aligned_kernel_size); // Format as hexadecimal
+			setenv("kern_len", kern_length_str);
 
             // Extract and process SquashFS size
             uint32_t bytes_used_low, bytes_used_high;
@@ -143,30 +150,30 @@ int process_spi_flash_data(struct spi_flash *flash) {
             memcpy(&bytes_used_high, buf + SQUASHFS_BYTES_USED_OFFSET + sizeof(uint32_t), sizeof(uint32_t));
             uint64_t bytes_used = ((uint64_t)bytes_used_high << 32) | bytes_used_low;
 
-            printf("Size at 0x%X: %llu bytes\n", addr, bytes_used);
+            //printf("Size at 0x%X: %llu bytes\n", addr, bytes_used);
 
             // Align and set SquashFS environment variables
             uint64_t aligned_bytes_used = align_to_erase_block(bytes_used);
             char size_str[32];
             sprintf(size_str, "%lluk", aligned_bytes_used / 1024);
-            setenv("rootmtd", size_str);
+            setenv("rootfs_size", size_str);
 
             // Set rootsize based on actual file size in memory
             uint64_t file_size = getenv_ulong("filesize", 16, 0);
             if (file_size > 0) {
                 uint64_t aligned_file_size = align_to_erase_block(file_size);
                 sprintf(size_str, "%lluk", aligned_file_size / 1024);
-                setenv("rootsize", size_str);
+                setenv("root_size", size_str);
             } else {
                 sprintf(size_str, "%lluk", aligned_bytes_used / 1024);
-                setenv("rootsize", size_str);
+                setenv("root_size", size_str);
             }
 
             return 0; // Success
         }
     }
 
-    printf("SquashFS not found within the search range\n");
+    printf("SquashFS not found.\n");
     return 1; // SquashFS not found
 }
 
--- include/configs/isvp_common.h
+++ include/configs/isvp_common.h
@@ -10,73 +10,7 @@
     #define CONFIG_SOC "t31"
 #endif
 
-#define CONFIG_BOOTCOMMAND "setenv setargs setenv bootargs ${bootargs}; run setargs; sf probe 0; sf read ${baseaddr} 0x50000 0x300000; bootm ${baseaddr}; reset"
-
-#define	CONFIG_SYS_HUSH_PARSER		1
-
-#if (defined(CONFIG_DDR2_128M) || defined(CONFIG_DDR3_128M))
-#define CONFIG_EXTRA_SETTINGS \
-    "totalmem=128M\0" \
-    "osmem=64M\0" \
-    "rmem=64M@0x4000000\0"
-#else
-#define CONFIG_EXTRA_SETTINGS \
-    "totalmem=64M\0" \
-    "osmem=40M\0" \
-    "rmem=24M@0x2800000\0"
-#endif
-
-/* USEFUL STUFF, BUT LATER...
-#ifdef CONFIG_SPL_MMC_SUPPORT
-	#define CONFIG_BOOTARGS "mem=\${osmem} rmem=\${rmem} init=/init root=/dev/mmcblk0p2 rootdelay=1"
-#elif defined(CONFIG_SFC_NOR)
-	#define CONFIG_BOOTARGS "mem=\${osmem} rmem=\${rmem} root=/dev/mtdblock3 rootfstype=squashfs init=/init mtdparts=jz_sfc:256k(boot),64k(env),2048k(kernel),5120k(rootfs),-(rootfs_data)"
-#elif defined(CONFIG_SFC_NAND)
-	#define CONFIG_BOOTARGS "mem=\${osmem} rmem=\${rmem} ip=off init=/init ubi.mtd=2 root=ubi0:rootfs ubi.mtd=3 rootfstype=ubifs rw"
-#endif
-*/
-
-#ifdef CONFIG_SPL_MMC_SUPPORT
-    #define CONFIG_BOOTCOMMAND "setenv setargs setenv bootargs ${bootargs}; run setargs; sf probe 0; mmc read ${baseaddr} 0x1800 0x3000; bootm ${baseaddr}"
-#endif
-#ifdef CONFIG_SFC_NOR
-    #define CONFIG_BOOTCOMMAND "setenv bootcmd ${bootcmdnor}; sf probe 0; saveenv; run bootcmd"
-#endif
-#ifdef CONFIG_SFC_NAND
-    #define CONFIG_BOOTCOMMAND "setenv setargs setenv bootargs ${bootargs}; run setargs; sf probe 0; sfcnand read 0x50000 0x200000 ${baseaddr} ; bootm ${baseaddr}; reset"
-#endif
-
-#define CONFIG_BOOTARGS "mem=\${osmem} rmem=\${rmem} console=ttyS1,115200n8 panic=20 root=/dev/mtdblock3 rootfstype=squashfs init=/init mtdparts=jz_sfc:256k(boot),64k(env),\\$kernsize(kernel),\\${rootmtd}(rootfs),-(rootfs_data) \${extras}"
-
-#define CONFIG_EXTRA_ENV_SETTINGS \
-    "baseaddr=0x80600000\0" \
-    "uknor8m=mw.b ${baseaddr} ff 1000000; tftpboot ${baseaddr} uImage.${soc} ; sf probe 0; sf erase 0x50000 0x200000; sf write ${baseaddr} 0x50000 ${filesize}\0" \
-    "uknor16m=mw.b ${baseaddr} ff 1000000; tftpboot ${baseaddr} uImage.${soc} ; sf probe 0; sf erase 0x50000 0x300000; sf write ${baseaddr} 0x50000 ${filesize}\0" \
-    "urnor8m=mw.b ${baseaddr} ff 1000000; tftpboot ${baseaddr} rootfs.squashfs.${soc} ; sf probe 0; sf erase 0x250000 0x500000; sf write ${baseaddr} 0x250000 ${filesize}\0" \
-    "urnor16m=mw.b ${baseaddr} ff 1000000; tftpboot ${baseaddr} rootfs.squashfs.${soc} ; sf probe 0; sf erase 0x350000 0xa00000; sf write ${baseaddr} 0x350000 ${filesize}\0" \
-    "uknand=mw.b ${baseaddr} ff 1000000; tftpboot ${baseaddr} uImage.${soc} ; nand erase 0x100000 0x300000; nand write ${baseaddr} 0x100000 0x300000\0" \
-    "urnand=mw.b ${baseaddr} ff 1000000; tftpboot ${baseaddr} rootfs.ubi.${soc} ; nand erase 0x400000 0x7c00000; nand write ${baseaddr} 0x400000 ${filesize}\0" \
-    "mtdparts=jz_sfc:256k(boot),64k(env),2048k(kernel),\\${rootmtd}(rootfs),-(rootfs_data)\0" \
-    "mtdpartsubi=setenv mtdparts jz_sfcnand:256k(boot),768k(wtf),3072k(kernel),-(ubi)\0" \
-    "mtdpartsnand=setenv mtdparts jz_sfcnand:256k(boot),768k(wtf),3072k(kernel),10240k(rootfs),-(rootfs_data)\0" \
-    "mtdpartsnor8m=setenv mtdparts jz_sfc:256k(boot),64k(env),2048k(kernel),5120k(rootfs),-(rootfs_data)\0" \
-    "mtdpartsnor16m=setenv mtdparts jz_sfc:256k(boot),64k(env),3072k(kernel),10240k(rootfs),-(rootfs_data)\0" \
-    "nfsroot=/srv/nfs/"CONFIG_SOC"\0" \
-    "bootargsnfs=mem=\${osmem} console=ttyS1,115200n8 panic=20 root=/dev/nfs rootfstype=nfs ip=${ipaddr}:::255.255.255.0::eth0 nfsroot=${serverip}:${nfsroot},v3,nolock rw \${extras}\0" \
-    "bootargsubi=mem=\${osmem} console=ttyS1,115200n8 panic=20 init=/init root=ubi0:rootfs rootfstype=ubifs ubi.mtd=3,2048 mtdparts=\${mtdparts} \${extras}\0" \
-    "bootnfs=setenv setargs setenv bootargs ${bootargsnfs}; run setargs; tftpboot ${baseaddr} uImage.${soc}; bootm ${baseaddr}\0" \
-    "bootcmdnand=setenv setargs setenv bootargs ${bootargs}; run setargs; nand read ${baseaddr} 0x100000 0x300000; bootm ${baseaddr}\0" \
-    "bootcmdubi=setenv setargs setenv bootargs ${bootargsubi}; run setargs; nand read ${baseaddr} 0x100000 0x300000; bootm ${baseaddr}\0" \
-    "bootcmdnor=sf probe 0; setenv setargs setenv bootargs ${bootargs}; run setargs; sf read ${baseaddr} 0x50000 0x300000; bootm ${baseaddr}\0" \
-    "setnand=run mtdpartsubi; setenv bootcmd ${bootcmdubi}; saveenv; reset\0" \
-    "setnor8m=run mtdpartsnor8m; setenv bootcmd ${bootcmdnor}; saveenv; reset\0" \
-    "setnor16m=run mtdpartsnor16m; setenv bootcmd ${bootcmdnor}; saveenv; reset\0" \
-    "restore=n\0" \
-    "soc="CONFIG_SOC"\0" \
-    CONFIG_EXTRA_SETTINGS
-
-#define CONFIG_SFC_MIN_ALIGN 0x10000
-
+#define CONFIG_SFC_MIN_ALIGN    0x10000
 #define CONFIG_ENV_OFFSET       0x40000
 #define CONFIG_ENV_SIZE         0x10000
 #define CONFIG_ENV_SECT_SIZE    0x10000
@@ -94,7 +28,7 @@
 #define CONFIG_SYS_MAXARGS      64
 #define CONFIG_SYS_NO_FLASH
 
-#define CONFIG_SYS_CBSIZE 2048 /* Console I/O Buffer Size */
+#define CONFIG_SYS_CBSIZE 2048
 #define CONFIG_SYS_PBSIZE (CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT) + 16)
 #define CONFIG_SYS_MALLOC_LEN (32 * 1024 * 1024)
 #define CONFIG_SYS_BOOTPARAMS_LEN (256 * 1024)
@@ -108,6 +42,7 @@
 #define CONFIG_CMD_FAT 1
 #define CONFIG_CMD_FS_GENERIC 1
 #define CONFIG_CMD_GPIO 1
+#define	CONFIG_SYS_HUSH_PARSER	1
 #define CONFIG_NETMASK 255.255.255.0
 #define CONFIG_GATEWAYIP 192.168.1.1
 #define CONFIG_SERVERIP 192.168.1.254
@@ -119,3 +54,38 @@
     #define CONFIG_CMD_SDUPDATE	1
 #endif
 #define CONFIG_CMD_SDSTART	1
+
+#define CONFIG_BOOTCOMMAND "setenv setargs setenv bootargs ${bootargs}; run setargs; sf probe 0; sf read ${baseaddr} 0x50000 0x300000; bootm ${baseaddr}"
+
+#if (defined(CONFIG_DDR2_128M) || defined(CONFIG_DDR3_128M))
+#define CONFIG_EXTRA_SETTINGS \
+    "totalmem=128M\0" \
+    "osmem=64M\0" \
+    "rmem=64M@0x4000000\0"
+#else
+#define CONFIG_EXTRA_SETTINGS \
+    "totalmem=64M\0" \
+    "osmem=40M\0" \
+    "rmem=24M@0x2800000\0"
+#endif
+
+#ifdef CONFIG_SPL_MMC_SUPPORT
+    #define CONFIG_BOOTCOMMAND "mmc rescan;setenv setargs setenv bootargs ${bootargs}; run setargs; mmc read ${baseaddr} 0x1800 0x3000; bootm ${baseaddr}"
+#endif
+#ifdef CONFIG_SFC_NOR
+    #define CONFIG_BOOTCOMMAND "sf probe;setenv setargs setenv bootargs ${bootargs}; run setargs; sf read ${baseaddr} 0x50000 \\${kern_len}; bootm ${baseaddr}"
+#endif
+#ifdef CONFIG_SFC_NAND
+    #define CONFIG_BOOTCOMMAND "sf probe;setenv setargs setenv bootargs ${bootargs}; run setargs; sfcnand read 0x50000 0x200000 ${baseaddr} ; bootm ${baseaddr}"
+#endif
+
+#define CONFIG_BOOTARGS "mem=\${osmem} rmem=\${rmem} console=\${serialport},\${baudrate}n8 panic=\${panic_timeout} root=/dev/mtdblock3 rootfstype=squashfs init=/init mtdparts=jz_sfc:256k(boot),64k(env),\\${kern_size}(kernel),\\${rootfs_size}(rootfs),-(rootfs_data)"
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+    "baseaddr=0x80600000\0" \
+    "panic_timeout=10\0" \
+    "serialport=ttyS1\0" \
+    "restore=n\0" \
+    "soc="CONFIG_SOC"\0" \
+    CONFIG_EXTRA_SETTINGS
+
