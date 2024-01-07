7ccaea4ab348e5d343944cdbd02aa64bdcc47fed
Paul Philippov <themactep@gmail.com>
2024-01-07 05:24:43 -0500

    refactor to fix collision of enums

--- common/cmd_gpio.c
+++ common/cmd_gpio.c
@@ -16,10 +16,10 @@
 #endif
 
 enum gpio_cmd {
-	GPIO_INPUT_,
-	GPIO_SET,
-	GPIO_CLEAR,
-	GPIO_TOGGLE,
+	GPIO_CMD_INPUT,
+	GPIO_CMD_SET,
+	GPIO_CMD_CLEAR,
+	GPIO_CMD_TOGGLE,
 };
 
 static int do_gpio(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
@@ -39,15 +39,16 @@ static int do_gpio(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	if (argc != 3)
  show_usage:
 		return CMD_RET_USAGE;
+
 	str_cmd = argv[1];
 	str_gpio = argv[2];
 
 	/* parse the behavior */
 	switch (*str_cmd) {
-		case 'i': sub_cmd = GPIO_INPUT;  break;
-		case 's': sub_cmd = GPIO_SET;    break;
-		case 'c': sub_cmd = GPIO_CLEAR;  break;
-		case 't': sub_cmd = GPIO_TOGGLE; break;
+		case 'i': sub_cmd = GPIO_CMD_INPUT;  break;
+		case 's': sub_cmd = GPIO_CMD_SET;    break;
+		case 'c': sub_cmd = GPIO_CMD_CLEAR;  break;
+		case 't': sub_cmd = GPIO_CMD_TOGGLE; break;
 		default:  goto show_usage;
 	}
 
@@ -63,15 +64,15 @@ static int do_gpio(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	}
 
 	/* finally, let's do it: set direction and exec command */
-	if (sub_cmd == GPIO_INPUT) {
+	if (sub_cmd == GPIO_CMD_INPUT) {
 		gpio_direction_input(gpio);
 		value = gpio_get_value(gpio);
 	} else {
 		switch (sub_cmd) {
-			case GPIO_SET:    value = 1; break;
-			case GPIO_CLEAR:  value = 0; break;
-			case GPIO_TOGGLE: value = !gpio_get_value(gpio); break;
-			default:          goto show_usage;
+			case GPIO_CMD_SET:    value = 1; break;
+			case GPIO_CMD_CLEAR:  value = 0; break;
+			case GPIO_CMD_TOGGLE: value = !gpio_get_value(gpio); break;
+			default: goto show_usage;
 		}
 		gpio_direction_output(gpio, value);
 	}
