ef713d8714e03d92ac3bd9655b205fac7633da40
gtxaspec <gtxent@gmail.com>
2024-01-17 17:41:11 -0800

    set ircut gpios to default as input

--- include/configs/isvp_common.h
+++ include/configs/isvp_common.h
@@ -239,6 +239,6 @@ CONFIG_GPIO_IRCUT_SETTINGS
 */
 
 #define CONFIG_GPIO_IRCUT_SETTINGS \
-"gpio_default_ircut=25o 26o 52o 53o 49o 50o\0"
+"gpio_default_ircut=25I 26I 52I 53I 49I 50I\0"
 
 #endif /*__CONFIG_ISVP_COMMON__*/
