c8b3991a2edb243e30a380b319fad8e2cabf18d6
gtxaspec <gtxent@gmail.com>
2024-01-22 07:48:19 -0800

    [no ci] add debug mode to build.sh

--- build.sh
+++ build.sh
@@ -6,19 +6,6 @@ export CROSS_COMPILE=mipsel-linux-gnu-
 
 declare -A cfg
 
-# NOR
-# T31N make isvp_t31_sfcnor
-# T31L make isvp_t31_sfcnor_lite
-# T31X make isvp_t31_sfcnor_ddr128M
-# T31A make isvp_t31a_sfcnor_ddr128M
-# T31AL make isvp_t31al_sfcnor_ddr128M
-# MMC
-# T31N make isvp_t31_msc0
-# T31L make isvp_t31_msc0_lite
-# T31X make isvp_t31_msc0_ddr128M
-# T31A make isvp_t31a_msc0_ddr128M
-# T31AL make isvp_t31al_msc0_ddr128M
-
 cfg[t10n]="isvp_t10_sfcnor"
 cfg[t10l]="isvp_t10_sfcnor_lite"
 cfg[t10n_msc0]="isvp_t10_msc0"
@@ -49,32 +36,50 @@ cfg[t31a_msc0]="isvp_t31a_msc0_ddr128M"
 cfg[t31al_msc0]="isvp_t31al_msc0_ddr128M"
 
 OUTPUT_DIR="./uboot_build"
+DEBUG_MODE=0  # Flag to indicate whether debug mode is active
 
-# Function to build a specific version
+# Function to build a specific version with possible debug mode
 build_version() {
 	local soc=$1
 	echo "Building U-Boot for ${soc}"
-	log="building-${soc}.log"; :>$log
-	make distclean    >>$log 2>&1
-	mkdir -p ${OUTPUT_DIR} >/dev/null 2>&1
-	make ${cfg[$soc]} >>$log 2>&1
-	make -j$(nproc)   >>$log 2>&1
-	cp u-boot-lzo-with-spl.bin ${OUTPUT_DIR}/u-boot-${soc}.bin
+
+	if [ "$DEBUG_MODE" -eq 1 ]; then
+		# Debug mode outputs directly to the console
+		make distclean
+		mkdir -p "${OUTPUT_DIR}" >/dev/null
+		make "${cfg[$soc]}"
+		make -j"$(nproc)"
+		cp u-boot-lzo-with-spl.bin "${OUTPUT_DIR}/u-boot-${soc}.bin"
+	else
+		# Normal mode, log to file
+		log="building-${soc}.log"; :>"$log"
+		make distclean    >>"$log" 2>&1
+		mkdir -p "${OUTPUT_DIR}" >/dev/null 2>&1
+		make "${cfg[$soc]}" >>"$log" 2>&1
+		make -j"$(nproc)"   >>"$log" 2>&1
+		cp u-boot-lzo-with-spl.bin "${OUTPUT_DIR}/u-boot-${soc}.bin"
+	fi
 }
 
 # Start timer
 SECONDS=0
 
+# Check if the last argument is "debug"
+if [ "${@: -1}" == "debug" ]; then
+	DEBUG_MODE=1
+	set -- "${@:1:$(($#-1))}"  # Remove the last argument
+fi
+
 # Check if an argument was provided
 if [ $# -eq 0 ]; then
 	# No argument, build all versions
 	for soc in "${!cfg[@]}"; do
-		build_version $soc
+		build_version "$soc"
 	done
 else
 	# Argument provided, build specific version
 	if [[ -n ${cfg[$1]} ]]; then
-		build_version $1
+		build_version "$1"
 	else
 		echo "Invalid argument: $1"
 		exit 1
