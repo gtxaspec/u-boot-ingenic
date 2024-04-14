dd776679be9a91722fda513e65e2c92bfaec499b
Paul Philippov <themactep@gmail.com>
2024-04-14 03:50:26 -0400

    SPDX headers

--- tools/ingenic-tools/mbr_creator.c
+++ tools/ingenic-tools/mbr_creator.c
@@ -4,20 +4,7 @@
  * Copyright (C) 2013 Ingenic Semiconductor Co.,Ltd
  * Author: Justin <ptkang@ingenic.cn>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307 USA
+ * SPDX-License-Identifier:	GPL-2.0+
  */
 
 #include <stdio.h>
@@ -127,7 +114,7 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	block[0x1fe] = 0x55;                                                                                                             
+	block[0x1fe] = 0x55;
 	block[0x1ff] = 0xaa;
 	memcpy(block+0x1c6,&tab_item[0].offset,sizeof(unsigned int));
 	memcpy(block+0x1d6,&tab_item[1].offset,sizeof(unsigned int));
