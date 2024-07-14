bb4961f6f89c34c5ad95fe9115b8aa064bf0e856
Paul Philippov <themactep@gmail.com>
2024-07-14 05:28:19 -0400

    do not print SPL title in a non-interactive environment

--- common/spl/spl.c
+++ common/spl/spl.c
@@ -260,8 +260,10 @@ void preloader_console_init(void)
 
 	gd->have_console = 1;
 
+#if !defined(CONFIG_FAST_BOOT) && !defined (CONFIG_SIMULATION) && !defined(CONFIG_YMODEM_NO_PRINTF)
 	puts("\n\nIngenic U-Boot Flex SPL " PLAIN_VERSION " (" U_BOOT_DATE " - " \
 			U_BOOT_TIME ")\n");
+#endif
 #ifdef CONFIG_SPL_DISPLAY_PRINT
 	spl_display_print();
 #endif
