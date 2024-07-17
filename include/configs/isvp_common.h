5eff295569ad5c987fad3b2fdf1ca20a2401805b
Paul Philippov <themactep@gmail.com>
2024-07-17 19:36:51 -0400

    exclusions for SPL builds

--- include/configs/isvp_common.h
+++ include/configs/isvp_common.h
@@ -706,14 +706,15 @@
 #define CONFIG_JZ_GPIO
 
 /************************ USB CONFIG ***************************/
+#if !defined(CONFIG_T31LC) && !defined(CONFIG_XIAOMI_SPL)
 #define CONFIG_CMD_USB
-
 #if defined(CONFIG_CMD_USB)
 #define CONFIG_USB_DWC2
 #define CONFIG_USB_DWC2_REG_ADDR	0x13500000
 #define CONFIG_USB_HOST_ETHER
 #define CONFIG_USB_ETHER_ASIX
 #define CONFIG_USB_STORAGE
+#endif
 #endif
 
 /**
